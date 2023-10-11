c6d6ecfa31c908f5091c1712e22b444a2700d3b2
Miguel Velasquez <veruiz.miguel@gmail.com>
2023-10-11 11:47:09 -0500

    feat(config): tsconfig, eslint and prettier added

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @typedef  {import("prettier").Config} PrettierConfig */
+
+/** @type { PrettierConfig | SortImportsConfig } */
+const config = {
+    printWidth: 80,
+    semi: true,
+    trailingComma: "all",
+    tabWidth: 2
+  };
+  
+  module.exports = config;
+  
